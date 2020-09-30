07fb7d1b1334accac9751e6a9160649af38a46d9
Victor Beccare <vbeccare@redventures.com>
2020-09-30 16:30:09 -0300

    Alteracoes finais

--- src/listar/listar-tarefas.js
+++ src/listar/listar-tarefas.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { A } from 'hookrouter';
-import { Table } from 'react-bootstrap';
+import { Table, Form } from 'react-bootstrap';
 import {FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faPlus} from '@fortawesome/free-solid-svg-icons'; 
 import ItensListaTarefas from './itens-lista-tarefas';
 import Paginacao from './paginacao';
+import Ordenacao from './ordenacao';
 
 
 function ListarTarefas() {
@@ -15,11 +16,25 @@ function ListarTarefas() {
     const [carregarTarefas, setCarregarTarefas] = useState(true);
     const [totalItems, setTotalItems] = useState(0);
     const [paginaAtual, setPaginaAtual] = useState(1);
+    const [ordenarAsc, setOrdenarAsc] = useState(false);
+    const [ordenarDesc, setOrdenarDesc] = useState(false);
+    const [filtroTarefa, setFiltroTarefa] = useState('');
 
     useEffect(() =>{
         function obterTarefas(){
             const tarefasDb = localStorage['tarefas'];
             let listaTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            // filtrar
+            listaTarefas = listaTarefas.filter(
+                t => t.nome.toLowerCase().indexOf(filtroTarefa.toLowerCase()) >= 0
+            );
+            // ordenar
+            if (ordenarAsc) {
+                listaTarefas.sort( (t1, t2) => (t1.nome.toLowerCase() > t2.nome.toLowerCase() ? 1 : -1 ));
+            }else if(ordenarDesc){
+                listaTarefas.sort( (t1, t2) => (t1.nome.toLowerCase() < t2.nome.toLowerCase() ? 1 : -1 ));
+            }
+            // paginar
             setTotalItems(listaTarefas.length);
             // divisao de cada tarefa por pagina
             setTarefas(listaTarefas.splice((paginaAtual - 1) * ITENS_POR_PAG, ITENS_POR_PAG ));
@@ -30,10 +45,28 @@ function ListarTarefas() {
         obterTarefas();
         setCarregarTarefas(false);
     }
-    }, [carregarTarefas, paginaAtual]);
+    }, [carregarTarefas, paginaAtual, ordenarDesc, ordenarAsc,filtroTarefa]);
 
     function handleMudarPagina(pagina){
         setPaginaAtual(pagina);
+        setCarregarTarefas(true);
+    }
+    function handleOrdenar(e){
+        e.preventDefault();
+        if(!ordenarAsc && !ordenarDesc){
+            setOrdenarAsc(true);
+            setOrdenarDesc(false);
+        }else if(ordenarAsc) {
+            setOrdenarAsc(false);
+            setOrdenarDesc(true);
+        } else {
+            setOrdenarDesc(false);
+            setOrdenarAsc(false);
+        }
+        setCarregarTarefas(true);
+    }
+    function handleFiltrar(e){
+        setFiltroTarefa(e.target.value);
         setCarregarTarefas(true);
     }
 
@@ -43,7 +76,15 @@ function ListarTarefas() {
         <Table striped bordered hover responsive data-testid="tabela">
             <thead>
                 <tr>
-                    <th>Tarefa</th>
+                    <th>
+                        <a href='/' onClick={handleOrdenar}>
+                        Tarefa
+                        &nbsp;
+                        <Ordenacao
+                            ordenarAsc={ordenarAsc}
+                            ordenarDesc={ordenarDesc} />
+                        </a>
+                    </th>
                     <th>
                         <A href="/cadastrar"
                         className=" btn btn-success btn-sm"
@@ -52,6 +93,19 @@ function ListarTarefas() {
                              &nbsp;
                             Nova tarefa
                         </A>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <Form.Control
+                        type='text'
+                        value={filtroTarefa}
+                        onChange={handleFiltrar}
+                        data-testid='txt-tarefa'
+                        className='filtro-tarefa'/>
+                    </th>
+                    <th>
+                        &nbsp;
                     </th>
                 </tr>
             </thead>
--- src/listar/paginacao.test.js
+++ src/listar/paginacao.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Paginacao from './paginacao';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Teste do componente de paginacao', () =>{
@@ -16,5 +16,17 @@ describe('Teste do componente de paginacao', () =>{
             mudarPagina={() => false }/>, div);
          ReactDOM.unmountComponentAtNode(div);
     });
-
+    it('deve exibir a paginação contendo 3 paginas', () => {
+        const { getByTestId } = render(
+            <Paginacao
+                totalItems={15}
+                itemsPorPagina={5}
+                paginaAtual={1}
+                mudarPagina={ () => false} />
+        );
+        const paginacao = getByTestId('paginacao');
+        expect(paginacao).toHaveTextContent('1');
+        expect(paginacao).toHaveTextContent('2');
+        expect(paginacao).toHaveTextContent('3');
+    })
 });
